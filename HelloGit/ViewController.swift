0082b6f102aa025c29584ac023f0b089e54009c4
hero1981 <hero_ideal@163.com>
2016-07-14 15:32:52 +0800

    Initial Commit

---
+++ HelloGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloGit
+//
+//  Created by wuxiaofei on 16/7/14.
+//  Copyright © 2016年 hero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
